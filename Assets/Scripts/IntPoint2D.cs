c47155a1d3a74334f6b77013186443039f983d30
Jeremiah Risinger <risinger.jeremiah@gmail.com>
2020-03-28 00:41:59 -0500

    uhm i think i did it right...

---
+++ Assets/Scripts/IntPoint2D.cs
@@ -0,0 +1,52 @@
+using System;
+	
+public struct IntPoint2D
+{
+	public int xCoord;
+	public int yCoord;
+
+	public static IntPoint2D zero = new IntPoint2D(0,0);
+
+	public IntPoint2D(int x, int y)
+	{
+		xCoord = x;
+		yCoord = y;
+	}
+
+	public static IntPoint2D operator +(IntPoint2D lhs, IntPoint2D rhs)
+	{
+		return new IntPoint2D(lhs.xCoord+rhs.xCoord,lhs.yCoord+rhs.yCoord);
+	}
+
+	public static bool operator==(IntPoint2D lhs, IntPoint2D rhs)
+	{
+		return (lhs.xCoord == rhs.xCoord && lhs.yCoord == rhs.yCoord);
+	}
+
+	public static bool operator!=(IntPoint2D lhs, IntPoint2D rhs)
+	{
+		return !(lhs == rhs);
+	}
+
+	public override bool Equals(object obj)
+	{
+		if (!(obj is IntPoint2D))
+			return false;
+		else 
+		{
+			IntPoint2D rhs = (IntPoint2D) obj;
+			return this.xCoord == rhs.xCoord && this.yCoord == rhs.yCoord;
+		}
+	}
+
+	public override int GetHashCode()
+	{
+		return this.xCoord+this.yCoord;
+	}
+
+	public override string ToString ()
+	{
+		return "(" + this.xCoord.ToString() + "," + this.yCoord.ToString() + ")";
+	}
+}
+
